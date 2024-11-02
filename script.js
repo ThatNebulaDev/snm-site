faa6a518c933e0e5c4eb7f4d158839c66d45b13a
ThatNebulaDev <nicolas.bzutterman@gmail.com>
2024-11-02 16:33:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         window.location.href = this.getAttribute('href');
     });
 });
+
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.getElementById("nav-links");
+
+mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
